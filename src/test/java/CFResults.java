6c7212c5cdf4e5ab4242fdcfcb1e90fd44a4b773
kristiiinakuz <48164501+kristinakuzmich@users.noreply.github.com>
2023-03-03 14:39:46 +0300

    Create CFResults.java

--- src/test/java/CFResults.java
+++ src/test/java/CFResults.java
@@ -2,7 +2,7 @@ import com.codeborne.selenide.Condition;
 import static com.codeborne.selenide.Selenide.$x;
 import static com.codeborne.selenide.Selenide.sleep;
 public class CFResults {
-    public void isExtectedResult(String text){
+    public void isExpectedResult(String text){
         $x("//span[text()='Кузьмич М.И.']")
                 .shouldBe(Condition.exactText(text));
     }
