2434588a8da5ca3a6c7dcf17e57ce7143a35e996
kristiiinakuz <48164501+kristinakuzmich@users.noreply.github.com>
2023-03-03 15:05:43 +0300

    Update CFResults.java

--- src/test/java/CFResults.java
+++ src/test/java/CFResults.java
@@ -2,8 +2,13 @@ import com.codeborne.selenide.Condition;
 import static com.codeborne.selenide.Selenide.$x;
 import static com.codeborne.selenide.Selenide.sleep;
 public class CFResults {
-    public void isExpectedResult(String text){
+    public void isExpectedName(String text){
         $x("//span[text()='Кузьмич М.И.']")
                 .shouldBe(Condition.exactText(text));
     }
+    public void isExpectedText(String text){
+        $x("//div[@class='ui-growl-message']//span[text()='Неверное имя пользователя или пароль.']")
+                .shouldBe(Condition.exactText(text));
+    }
+
 }
