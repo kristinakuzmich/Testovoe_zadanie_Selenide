3c979cf0fd3d1893755d379510ae3fead3c4b93a
kuzmich <kristina.kuzmich.33@gmail.com>
2023-03-03 15:02:19 +0300

    Изменены некоторые функции

--- src/test/java/CFResults.java
+++ src/test/java/CFResults.java
@@ -2,9 +2,13 @@ import com.codeborne.selenide.Condition;
 import static com.codeborne.selenide.Selenide.$x;
 import static com.codeborne.selenide.Selenide.sleep;
 public class CFResults {
-    public void isExtectedResult(String text){
+    public void isExpectedName(String text){
         $x("//span[text()='Кузьмич М.И.']")
                 .shouldBe(Condition.exactText(text));
-        sleep(3000);
     }
+    public void isExpectedText(String text){
+        $x("//div[@class='ui-growl-message']//span[text()='Неверное имя пользователя или пароль.']")
+                .shouldBe(Condition.exactText(text));
+    }
+
 }
--- src/test/java/SingletonDriver.java
+++ src/test/java/SingletonDriver.java
@@ -2,6 +2,7 @@ import com.codeborne.selenide.Selenide;
 public class SingletonDriver {
     private static SingletonDriver instanceOfSingletonDriver=null;
     private SingletonDriver(){
+
         Selenide.open("http://192.168.166.198:8080/fos/");
     }
     public static SingletonDriver getInstanceOfSingletonDriver(){
